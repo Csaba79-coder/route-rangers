3e3827dbfd8b6b658af23cac1e42055484bc5237
Csaba Vadász <csabavadasz79@gmail.com>
2024-09-21 18:24:31 +0200

    Initial commit

---
+++ src/main/java/hu/pannonuni/routerangers/RouteRangersApplication.java
@@ -0,0 +1,13 @@
+package hu.pannonuni.routerangers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteRangersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteRangersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/pannonuni/routerangers/RouteRangersApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pannonuni.routerangers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteRangersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
