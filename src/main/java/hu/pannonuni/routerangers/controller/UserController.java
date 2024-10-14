6b0e55a873e632a4a866d825b33b0291716f42a4
Csaba Vadász <csabavadasz79@gmail.com>
2024-10-14 09:34:20 +0200

    create example for using api contract yaml file, update the pom xml for using OpenAPI Generator 7

--- src/main/java/hu/pannonuni/routerangers/controller/UserController.java
+++ src/main/java/hu/pannonuni/routerangers/controller/UserController.java
@@ -1,21 +1,21 @@
 package hu.pannonuni.routerangers.controller;
 
-import io.swagger.model.UserModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class UserController implements io.swagger.api.UserApi {
+public class UserController implements hu.pannonuni.api.UsersApi {
 
     @Override
-    public ResponseEntity<UserModel> createUser(UserModel body) {
-        return null;
+    public ResponseEntity<hu.pannonuni.model.UserModel> createUser(hu.pannonuni.model.UserRegistrationModel userRegistrationModel) {
+        return ResponseEntity.status(201).body(new hu.pannonuni.model.UserModel());
     }
 
     @Override
-    public ResponseEntity<List<UserModel>> renderAllUsers() {
-        return null;
+    public ResponseEntity<List<hu.pannonuni.model.UserModel>> renderAllUsers() {
+        return ResponseEntity.ok(new ArrayList<>());
     }
 }
