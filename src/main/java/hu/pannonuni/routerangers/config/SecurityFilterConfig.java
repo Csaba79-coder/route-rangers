e0019ee30ecdbfbcc18a4326647dfcb616da3d7e
adamkaj <adam2000buzas@gmail.com>
2025-01-05 13:45:09 +0100

    add cors config

--- src/main/java/hu/pannonuni/routerangers/config/SecurityFilterConfig.java
+++ src/main/java/hu/pannonuni/routerangers/config/SecurityFilterConfig.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,15 +23,14 @@ public class SecurityFilterConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
+                .cors(withDefaults()) // Engedélyezi a CORS szabályokat
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/optimize", "/api/**", "/auth/truck-packing").permitAll()
                         .requestMatchers("/auth/**").hasRole("USER")
                         .anyRequest().authenticated()
                 )
-
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // Kikapcsolja a CSRF-t
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
